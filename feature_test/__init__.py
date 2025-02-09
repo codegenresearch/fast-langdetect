61f883fdcfa662b7fcea62fb8e14664aeb112d5f
venkatkrishna <venkatkrishna@moe>
2025-02-09 23:23:43 +0100

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2024/1/18 上午11:41
 # @Author  : sudoskys
+# @File    : __init__.py.py
+# @Software: PyCharm
+import warnings
+from fast_langdetect import detect as deprecated_detect, detect_multilingual as deprecated_detect_multilingual, detect_langs as deprecated_detect_langs
+from fast_langdetect import parse_sentence
 
+def detect_languages(text, low_memory=False):
+    warnings.warn("The `detect_languages` function is the new consolidated method for language detection. Old functions are deprecated.", DeprecationWarning)
+    if low_memory:
+        return deprecated_detect_multilingual(text)
+    else:
+        return deprecated_detect_langs(text)
 
-from fast_langdetect import detect, detect_multilingual, detect_language
+print(parse_sentence("你好世界"))
+print(parse_sentence("你好世界！Hello, world！Привет, мир！"))
+print(detect_languages("Hello, world!你好世界!Привет, мир!", low_memory=True))
 
-# 测试繁体，简体，日文，英文，韩文，法文，德文，西班牙文
-
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
-# [{'lang': 'ja', 'score': 0.32009604573249817}, {'lang': 'uk', 'score': 0.27781224250793457}, {'lang': 'zh', 'score': 0.17542070150375366}, {'lang': 'sr', 'score': 0.08751443773508072}, {'lang': 'bg', 'score': 0.05222449079155922}]
-print(detect("hello world"))
-
-print(detect_language("Привет, мир!"))
-print(detect_language("你好世界"))
-print(detect_language("こんにちは世界"))
-print(detect_language("안녕하세요 세계"))
-print(detect_language("Bonjour le monde"))
-print(detect_language("Hallo Welt"))
-print(detect_language("Hola mundo"))
-print(detect_language("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+print(detect_languages("hello world"))
+print(detect_languages("Привет, мир!"))
