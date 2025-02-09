5381a27c35a9425e8a664bcbd5e2ab527b8daaac
venkatkrishna <venkatkrishna@moe>
2025-02-09 23:35:27 +0100

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -3,20 +3,15 @@
 # @Author  : sudoskys
 # @File    : __init__.py.py
 # @Software: PyCharm
-import warnings
-from fast_langdetect import detect as deprecated_detect, detect_multilingual as deprecated_detect_multilingual, detect_langs as deprecated_detect_langs
-from fast_langdetect import parse_sentence
+from fast_langdetect import detect, detect_multilingual, parse_sentence
 
-def detect_languages(text, low_memory=False):
-    warnings.warn("The `detect_languages` function is the new consolidated method for language detection. Old functions are deprecated.", DeprecationWarning)
-    if low_memory:
-        return deprecated_detect_multilingual(text)
-    else:
-        return deprecated_detect_langs(text)
+# Testing parse_sentence function
+print(parse_sentence("你好世界"))  # Testing Chinese
+print(parse_sentence("你好世界！Hello, world！Привет, мир！"))  # Testing mixed languages
 
-print(parse_sentence("你好世界"))
-print(parse_sentence("你好世界！Hello, world！Привет, мир！"))
-print(detect_languages("Hello, world!你好世界!Привет, мир!", low_memory=True))
+# Testing detect_multilingual function
+print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))  # Testing mixed languages
 
-print(detect_languages("hello world"))
-print(detect_languages("Привет, мир!"))
+# Testing detect function
+print(detect("hello world"))  # Testing English
+print(detect("Привет, мир!"))  # Testing Russian
