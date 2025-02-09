cdaeab0467a82d025b44ee19a375e4000cc43acd
venkatkrishna <venkatkrishna@moe>
2025-02-10 00:37:28 +0100

    Reset to 37aca0398d08f882a9990207166a131366b3fd42 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -8,6 +8,7 @@ from fast_langdetect import detect, detect_multilingual, detect_language
 
 # Expected output: [{'lang': 'ja', 'score': 0.32009604573249817}, {'lang': 'uk', 'score': 0.27781224250793457}, {'lang': 'zh', 'score': 0.17542070150375366}, {'lang': 'sr', 'score': 0.08751443773508072}, {'lang': 'bg', 'score': 0.05222449079155922}]
 print(detect_multilingual("Hello, world!你好世界!Привет, мир!", low_memory=False))
+print(detect_multilingual("Hello, world!你好世界!Привет, мир!", low_memory=False))
 print(detect("hello world"))
 print(detect("你好世界"))
 
