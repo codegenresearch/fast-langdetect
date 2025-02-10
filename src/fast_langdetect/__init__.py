45ff957bd07a1359a6e846b80b5ff73e77f4bd9e
venkatkrishna <venkatkrishna@moe>
2025-02-11 00:55:10 +0100

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- src/fast_langdetect/__init__.py
+++ src/fast_langdetect/__init__.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .ft_detect import detect, detect_multilingual
-
-# Deprecation warning for detect_langs
-import warnings
-warnings.warn("The function 'detect_langs' is deprecated and will be removed in future versions.", DeprecationWarning)
+from .ft_detect import detect, detect_langs, detect_multilingual, detect_language  # noqa: F401
