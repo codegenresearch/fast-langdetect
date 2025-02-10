b8e0d38c3a70f20c24bf7a94599e05fb856e7462
venkatkrishna <venkatkrishna@moe>
2025-02-10 20:40:13 +0100

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -3,20 +3,27 @@
 # @Author  : sudoskys
 # @File    : __init__.py.py
 # @Software: PyCharm
-import warnings
-from fast_langdetect import detect as deprecated_detect, detect_multilingual as deprecated_detect_multilingual, detect_langs as deprecated_detect_langs
-from fast_langdetect import parse_sentence
+from fast_langdetect import detect, detect_multilingual, detect_langs, parse_sentence
 
-def detect_languages(text, low_memory=False):
-    warnings.warn("The `detect_languages` function is the new consolidated method for language detection. Old functions are deprecated.", DeprecationWarning)
-    if low_memory:
-        return deprecated_detect_multilingual(text)
-    else:
-        return deprecated_detect_langs(text)
+# Testing sentence parsing
+print("Parsing sentence '你好世界':", parse_sentence("你好世界"))
+print("Parsing sentence '你好世界！Hello, world！Привет, мир！':", parse_sentence("你好世界！Hello, world！Привет, мир！"))
 
-print(parse_sentence("你好世界"))
-print(parse_sentence("你好世界！Hello, world！Привет, мир！"))
-print(detect_languages("Hello, world!你好世界!Привет, мир!", low_memory=True))
+# Testing multilingual detection
+print("Detecting languages in 'Hello, world!你好世界!Привет, мир!':", detect_multilingual("Hello, world!你好世界!Привет, мир!"))
 
-print(detect_languages("hello world"))
-print(detect_languages("Привет, мир!"))
+# Testing single language detection
+print("Detecting language in 'hello world':", detect("hello world"))
+print("Detecting languages in 'Привет, мир!':", detect_langs("Привет, мир!"))
+
+# Additional test cases to showcase functionality
+print("Detecting language in 'こんにちは世界':", detect("こんにちは世界"))
+print("Detecting languages in 'こんにちは世界':", detect_langs("こんにちは世界"))
+print("Detecting language in '안녕하세요 세계':", detect("안녕하세요 세계"))
+print("Detecting languages in '안녕하세요 세계':", detect_langs("안녕하세요 세계"))
+print("Detecting language in 'Bonjour le monde':", detect("Bonjour le monde"))
+print("Detecting languages in 'Bonjour le monde':", detect_langs("Bonjour le monde"))
+print("Detecting language in 'Hallo Welt':", detect("Hallo Welt"))
+print("Detecting languages in 'Hallo Welt':", detect_langs("Hallo Welt"))
+print("Detecting language in 'これらの機構主辦の課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等':", detect("これらの機構主辦の課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+print("Detecting languages in 'これらの機構主辦の課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等':", detect_langs("これらの機構主辦の課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
