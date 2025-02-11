7e398adb1ef0da0c7de429d842dfa96188097da1
venkatkrishna <venkatkrishna@moe>
2025-02-11 01:17:25 +0100

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- src/fast_langdetect/__init__.py
+++ src/fast_langdetect/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .ft_detect import detect_language, detect_languages, detect_multilingual_languages
+from .ft_detect import detect, detect_langs, detect_multilingual, detect_language, detect_languages, detect_multilingual_languages  # noqa: F401
